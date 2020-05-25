a7f1a6df30f13c9bc0356aded08d8d4d16383311
Stephanie Diep <stephaniediep@outlook.com>
2020-05-25 17:31:26 -0400

    Created basic controllers and added index route for pantry items

---
+++ app/controllers/pantry_items_controller.rb
@@ -0,0 +1,17 @@
+class PantryItemsController < ApplicationController
+  def index
+    @pantry_items = PantryItem.where(user: current_user)
+  end
+
+  def create
+    @pantry_item = PantryItem.new
+    @pantry_item.user = current_user
+
+    if @pantry_item.save
+      redirect_to pantry "/users/:user_id/pantry_items"
+    else
+      # Redirect to pantry_items index page for now because don't know how to render popup
+      render "/users/:user_id/pantry_items"
+    end
+  end
+end
