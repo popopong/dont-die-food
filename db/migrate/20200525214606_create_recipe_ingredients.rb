96c07496497bd5e25eafca4094e984283883e9b4
Elie Hymowitz <ehymowitz@gmail.com>
2020-05-26 10:57:07 -0400

    Changed data to text datatype

--- db/migrate/20200525214606_create_recipe_ingredients.rb
+++ db/migrate/20200525214606_create_recipe_ingredients.rb
@@ -1,7 +1,7 @@
 class CreateRecipeIngredients < ActiveRecord::Migration[6.0]
   def change
     create_table :recipe_ingredients do |t|
-      t.string :data
+      t.text :data
       t.references :recipe, null: false, foreign_key: true
       t.references :ingredient, null: false, foreign_key: true
 
