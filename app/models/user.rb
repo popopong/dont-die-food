eb49092c9257ebc753f5b802a55329708f9d8394
Elie Hymowitz <ehymowitz@gmail.com>
2020-05-26 11:53:21 -0400

    Edited db, validated emails and ps, cloudinary config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :photo
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -8,7 +10,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :encrypted_password, presence: true
-
+  validates :email, presence: true, uniqueness: true,
+            format: { with: /\w+@\w+\.[a-z]{2,4}\z/, message: "Please enter a valid email address"}
+  validates :encrypted_password, presence: true, length: { minimum: 6 }
 end
