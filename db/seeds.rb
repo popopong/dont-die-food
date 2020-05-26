2d9ac435f91210f1cc4ebe958a811d77d3dcedd7
Stephanie Diep <stephaniediep@outlook.com>
2020-05-26 11:53:23 -0400

    Started seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "🧹 Cleaning database"
+Pantry_items.destroy_all
+Recipe_ingredients.destroy_all
+Ingredient.destroy_all
+Recipe.destroy_all
+User.destroy_all
+
+puts "🧑 Creating users..."
+elie = User.create!(first_name: "Elie", last_name: "Hymowitz", email: "elie@hello.com", encrypted_password: "123456")
+stephd = User.create!(first_name: "Stephanie", last_name: "Diep", email: "stephd@hello.com", encrypted_password: "123456")
+poyan = User.create!(first_name: "Poyan", last_name: "Ng", email: "poyan@hello.com", encrypted_password: "123456")
+stephbd = User.create!(first_name: "Stephanie", last_name: "BD", email: "stephbd@hello.com", encrypted_password: "123456")
+
+puts "👩‍🍳 Creating recipes..."
+
+puts "🍅 Creating ingredients..."
+
+puts "🔗 Linking ingredients and recipes together..."
+
+puts "🍚 Adding pantry items to each user's pantry..."
+
+puts "🎉 Successfully created 4 users!"
