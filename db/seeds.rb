14f6634b062fcf4362887bcad03b6b8f2aea4257
Stephanie Diep <stephaniediep@outlook.com>
2020-05-27 11:35:36 -0400

    Seeded more recipes in my db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ require 'openssl'
 # Run "rake db:seed:dump FILE=db/seed_dump.rb" to save seeds in db/seed_dump.rb
 # rails db:structure:dump to dump our data in a dump file
 
-# puts "🧹 Cleaning database"
+puts "🧹 Cleaning database"
 PantryItem.destroy_all
 # RecipeIngredient.destroy_all
 # Ingredient.destroy_all
@@ -20,7 +20,7 @@ stephd = User.create!(first_name: "Stephanie", last_name: "Diep", email: "stephd
 poyan = User.create!(first_name: "Poyan", last_name: "Ng", email: "poyan@hello.com", password: "1234567")
 stephbd = User.create!(first_name: "Stephanie", last_name: "BD", email: "stephbd@hello.com", password: "1234567")
 
-# puts "👩‍🍳 Creating recipes..."
+puts "👩‍🍳 Creating recipes..."
 # # Make three API calls to spooncular API
 
 # API call #1 : Store the recipe ID in a variable, i.e response, add a keyword into parameters JUST RETURN THE FIRST RESULT!!
@@ -33,7 +33,7 @@ def find_recipe_by_keyword(search_term)
 
   request = Net::HTTP::Get.new(url)
   response = http.request(request)
-  puts response.read_body
+  # puts response.read_body
   recipes_array = JSON.parse(response.read_body)["results"]
   get_recipe_ingredients_and_steps(recipes_array)
 end
@@ -53,7 +53,7 @@ def get_recipe_ingredients_and_steps(recipe_array)
 
     request = Net::HTTP::Get.new(url)
     response = http.request(request)
-    puts response.read_body
+    # puts response.read_body
     extended_ingredients = JSON.parse(response.read_body)
     # Stores extendedIngredient as JSON in ingredients_data column:
     if extended_ingredients && extended_ingredients.size > 0
@@ -70,7 +70,7 @@ def get_recipe_ingredients_and_steps(recipe_array)
 
     request = Net::HTTP::Get.new(url)
     response = http.request(request)
-    puts response.read_body
+    # puts response.read_body
     steps = JSON.parse(response.read_body)
     if steps && steps.size > 0
       steps = steps.first["steps"]
@@ -82,6 +82,8 @@ end
 
 # Create an array of keywords
 keywords = [{ name: "avocado", number: 5 }, { name: "apple", number: 5 }, { name: "pie", number: 5 }]
+# keywords = [{ name: "banana", number: 5 }, { name: "peach", number: 5 }, { name: "lemon", number: 5 }]
+# keywords = [{ name: "tomato", number: 5 }, { name: "cake", number: 5 }, { name: "cheese", number: 5 }, { name: "carrot", number: 5 }, { name: "eggplant", number: 5 }, { name: "pizza", number: 5 }]
 
 keywords.each do |keyword|
   find_recipe_by_keyword(keyword)
