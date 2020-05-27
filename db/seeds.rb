b73d08c4dd4646a9a7e8dfe8269e28f87eb960d9
Stephanie Diep <stephaniediep@outlook.com>
2020-05-27 15:01:48 -0400

    Seeded chatrooms and messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,8 @@ def iterable?(object)
 end
 
 puts "🧹 Cleaning database"
+Message.destroy_all
+Chatroom.destroy_all
 FoodTrade.destroy_all
 UserOwnedIngredient.destroy_all
 SavedRecipe.destroy_all
@@ -28,74 +30,74 @@ stephd = User.create!(first_name: "Stephanie", last_name: "Diep", email: "stephd
 poyan = User.create!(first_name: "Poyan", last_name: "Ng", email: "poyan@hello.com", password: "1234567")
 stephbd = User.create!(first_name: "Stephanie", last_name: "BD", email: "stephbd@hello.com", password: "1234567")
 
-# puts "👩‍🍳 Creating recipes..."
-# # # Make three API calls to spooncular API
+puts "👩‍🍳 Creating recipes..."
+# Make three API calls to spooncular API
 
-# # API call #1 : Store the recipe ID in a variable, i.e response, add a keyword into parameters JUST RETURN THE FIRST RESULT!!
-# def find_recipe_by_keyword(search_term)
-#   url = URI("https://api.spoonacular.com/recipes/search?query=#{search_term[:name]}&number=#{search_term[:number]}&apiKey=#{ENV["SPOONACULAR_APIKEY"]}")
+# API call #1 : Store the recipe ID in a variable, i.e response, add a keyword into parameters JUST RETURN THE FIRST RESULT!!
+def find_recipe_by_keyword(search_term)
+  url = URI("https://api.spoonacular.com/recipes/search?query=#{search_term[:name]}&number=#{search_term[:number]}&apiKey=#{ENV["SPOONACULAR_APIKEY"]}")
 
-#   http = Net::HTTP.new(url.host, url.port)
-#   http.use_ssl = true
-#   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-#   request = Net::HTTP::Get.new(url)
-#   response = http.request(request)
-#   # puts response.read_body
-#   recipes_array = JSON.parse(response.read_body)["results"]
-#   get_recipe_ingredients_and_steps(recipes_array)
-# end
+  request = Net::HTTP::Get.new(url)
+  response = http.request(request)
+  # puts response.read_body
+  recipes_array = JSON.parse(response.read_body)["results"]
+  get_recipe_ingredients_and_steps(recipes_array)
+end
 
-# def get_recipe_ingredients_and_steps(recipe_array)
-#   # Get the recipe ID
-#   recipe_array.each do |recipe|
-#     # API call #2 : Store the recipe ingredients in another variable, i.e response
-#     new_recipe = Recipe.new(title: recipe["title"])
-#     next if Recipe.find_by(title: recipe["title"])
-#     recipe_id = recipe["id"]
-#     # Make another API call to get ingredients
-#     url = URI("https://api.spoonacular.com/recipes/#{recipe_id}/information?apiKey=#{ENV["SPOONACULAR_APIKEY"]}")
-#     http = Net::HTTP.new(url.host, url.port)
-#     http.use_ssl = true
-#     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+def get_recipe_ingredients_and_steps(recipe_array)
+  # Get the recipe ID
+  recipe_array.each do |recipe|
+    # API call #2 : Store the recipe ingredients in another variable, i.e response
+    new_recipe = Recipe.new(title: recipe["title"])
+    next if Recipe.find_by(title: recipe["title"])
+    recipe_id = recipe["id"]
+    # Make another API call to get ingredients
+    url = URI("https://api.spoonacular.com/recipes/#{recipe_id}/information?apiKey=#{ENV["SPOONACULAR_APIKEY"]}")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-#     request = Net::HTTP::Get.new(url)
-#     response = http.request(request)
-#     # puts response.read_body
-#     extended_ingredients = JSON.parse(response.read_body)
-#     # Stores extendedIngredient as JSON in ingredients_data column:
-#     if extended_ingredients && extended_ingredients.size > 0
-#       extended_ingredients = extended_ingredients["extendedIngredients"]
-#       new_recipe.ingredients_data = extended_ingredients
-#     end
+    request = Net::HTTP::Get.new(url)
+    response = http.request(request)
+    # puts response.read_body
+    extended_ingredients = JSON.parse(response.read_body)
+    # Stores extendedIngredient as JSON in ingredients_data column:
+    if extended_ingredients && extended_ingredients.size > 0
+      extended_ingredients = extended_ingredients["extendedIngredients"]
+      new_recipe.ingredients_data = extended_ingredients
+    end
 
-#     # API call #3 : Store the recipe steps in another variable, i.e response
-#     # Make another API call to get ingredients
-#     url = URI("https://api.spoonacular.com/recipes/#{recipe_id}/analyzedInstructions?apiKey=#{ENV["SPOONACULAR_APIKEY"]}")
-#     http = Net::HTTP.new(url.host, url.port)
-#     http.use_ssl = true
-#     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    # API call #3 : Store the recipe steps in another variable, i.e response
+    # Make another API call to get ingredients
+    url = URI("https://api.spoonacular.com/recipes/#{recipe_id}/analyzedInstructions?apiKey=#{ENV["SPOONACULAR_APIKEY"]}")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-#     request = Net::HTTP::Get.new(url)
-#     response = http.request(request)
-#     # puts response.read_body
-#     steps = JSON.parse(response.read_body)
-#     if steps && steps.size > 0
-#       steps = steps.first["steps"]
-#       new_recipe.steps_data = steps
-#       new_recipe.save!
-#     end
-#   end
-# end
+    request = Net::HTTP::Get.new(url)
+    response = http.request(request)
+    # puts response.read_body
+    steps = JSON.parse(response.read_body)
+    if steps && steps.size > 0
+      steps = steps.first["steps"]
+      new_recipe.steps_data = steps
+      new_recipe.save!
+    end
+  end
+end
 
-# # Create an array of keywords
-# keywords = [{ name: "avocado", number: 5 }, { name: "apple", number: 5 }, { name: "pie", number: 5 }]
-# # keywords = [{ name: "banana", number: 5 }, { name: "peach", number: 5 }, { name: "lemon", number: 5 }]
-# # keywords = [{ name: "tomato", number: 5 }, { name: "cake", number: 5 }, { name: "cheese", number: 5 }, { name: "carrot", number: 5 }, { name: "eggplant", number: 5 }, { name: "pizza", number: 5 }]
+# Create an array of keywords
+keywords = [{ name: "avocado", number: 5 }, { name: "apple", number: 5 }, { name: "pie", number: 5 }]
+# keywords = [{ name: "banana", number: 5 }, { name: "peach", number: 5 }, { name: "lemon", number: 5 }]
+# keywords = [{ name: "tomato", number: 5 }, { name: "cake", number: 5 }, { name: "cheese", number: 5 }, { name: "carrot", number: 5 }, { name: "eggplant", number: 5 }, { name: "pizza", number: 5 }]
 
-# keywords.each do |keyword|
-#   find_recipe_by_keyword(keyword)
-# end
+keywords.each do |keyword|
+  find_recipe_by_keyword(keyword)
+end
 
 puts "🍅 Creating ingredients..."
 # loop through recipes and check if ingredient exists, if not create new ingredient
@@ -201,6 +203,14 @@ FoodTrade.find_or_create_by(user_owned_ingredient: stephd.user_owned_ingredients
 FoodTrade.find_or_create_by(user_owned_ingredient: stephd.user_owned_ingredients.second, location: "705 Saint-Catherine St W, Montreal, Quebec H3B 4G5", amount: 5, description: "Looking for garlic for one of your recipes? I got too many garlics sitting around!")
 
 # Poyan's food trades
-FoodTrade.find_or_create_by(user_owned_ingredient: poyan.user_owned_ingredients.second, location: "329, 327 Avenue Melville, Westmount, Quebec H3Z 2J7", amount: 1, unit: "bottle", description: "Looking for garlic for one of your recipes? I got too many garlics sitting around!")
+FoodTrade.find_or_create_by(user_owned_ingredient: poyan.user_owned_ingredients.second, location: "329, 327 Avenue Melville, Westmount, Quebec H3Z 2J7", amount: 1, unit: "bottle", description: "Too much olive oil here in my house, need to get rid of them!")
 
-puts "🎉 Successfully created users, recipes, ingredients, recipe_ingredients, pantry items, saved_recipes!"
+puts "👋 Creating chatrooms..."
+Chatroom.find_or_create_by(food_trade: elie.food_trades.first, starred: true)
+Chatroom.find_or_create_by(food_trade: stephd.food_trades.first)
+
+puts "💬 Creating messages..."
+Message.find_or_create_by(content: "Hi Elie! I would like to trade some lemon zest for an apple pie I'm planning to make this weekend!", user: poyan, chatroom: Chatroom.first)
+Message.find_or_create_by(content: "Hi Stephanie! I need 1 box of greek yogurt, thanks!", user: stephbd, chatroom: Chatroom.first)
+
+puts "🎉 Successfully created users, recipes, ingredients, recipe_ingredients, pantry items, saved_recipes, user_owned_ingredients, food_trades, chatrooms and messages !"
