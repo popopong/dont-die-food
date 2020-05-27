835e2491cc83132c45a4548d41d25526e8828bc9
Elie Hymowitz <ehymowitz@gmail.com>
2020-05-27 14:59:48 -0400

    User Owned Ingredients Controller

--- app/controllers/user_owned_ingredients_controller.rb
+++ app/controllers/user_owned_ingredients_controller.rb
@@ -1,7 +1,25 @@
 class UserOwnedIngredientsController < ApplicationController
   def create
+    @user_owned_ingredient = UserOwnedIngredient.find(params[:id])
+    @user_owned_ingredient.user = current_user
+    if @saved_recipe.save
+      # Good flash notice goes here
+      redirect_to "recipes/show"
+    else
+      # Bad flash notice goes here
+      redirect_to "recipes/show"
+    end
   end
 
   def destroy
+    @user_owned_ingredient = UserOwnedIngredient.find(params[:id])
+    @user_owned_ingredient.user = current_user
+    if @saved_recipe.destroy
+      # Good flash notice goes here
+      redirect_to "recipes/show"
+    else
+      # Bad flash notice goes here
+      redirect_to "recipes/show"
+    end
   end
 end
