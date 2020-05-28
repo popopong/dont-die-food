3294d81e26c4a2cecf2edaf90b926f9659f0d465
Elie Hymowitz <ehymowitz@gmail.com>
2020-05-28 10:57:36 -0400

    Added photo to ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_145303) do
+ActiveRecord::Schema.define(version: 2020_05_28_145647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_145303) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo"
   end
 
   create_table "messages", force: :cascade do |t|
