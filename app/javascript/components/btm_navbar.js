8b8ea1bf7156b725199e30836d3716f6a106081c
Poyan Ng <poyanng.npy@gmail.com>
2020-05-31 18:52:01 -0400

    add hover effect tmr

--- app/javascript/components/btm_navbar.js
+++ app/javascript/components/btm_navbar.js
@@ -1,8 +1,8 @@
 const btmNavbar = () => {
   window.onscroll = function() {myFunction()};
 
-  var navbar = document.getElementById("navbar");
-  var sticky = navbar.offsetBottom;
+  var navbar = document.getElementById("btm-navbar");
+  var sticky = navbar.offsetTop;
 
   function myFunction() {
     if (window.pageYOffset >= sticky) {
@@ -11,6 +11,16 @@ const btmNavbar = () => {
       navbar.classList.remove("sticky");
     }
   }
+
+  const icons = document.querySelectorAll(".btm-nav-icons");
+  icons.forEach((icon) => {
+    icon.addEventListener("click", (event) => {
+    event.preventDefault();
+    event.currentTarget.classList.add("active");
+      // event.currentTarget.classList.add("active");
+      console.log(event.currentTarget);
+    });
+  });
 };
 
 export { btmNavbar };
