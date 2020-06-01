577a61a50dacd9702dd5ea5383539dc619b7f83d
Poyan Ng <poyanng.npy@gmail.com>
2020-05-31 23:59:29 -0400

    styled the trade-form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,13 +27,14 @@ import "bootstrap";
 import { typed } from "../components/typed_js"
 import { btmNavbar } from "../components/btm_navbar"
 import { initMapbox } from '../plugins/init_mapbox';
+import { initSelect2 } from '../plugins/init_select2';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  initSelect2();
   typed();
   btmNavbar();
   initMapbox();
