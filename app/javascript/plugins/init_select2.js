10a5095c5fe41533935d65bf60ad13736c567a8a
Poyan Ng <poyanng.npy@gmail.com>
2020-06-02 18:27:33 -0400

    deleted comments

--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -8,22 +8,10 @@ const initSelect2 = () => {
   $('#ingredients').select2({
     sorter: data => data.sort((a, b) => a.text.localeCompare(b.text)),
     });
-  // $('.food_trade_select').each((i, e) => { 
-  //   // $(e).select2();
-  //   // console.log(i, e);
-  //   });
+
   document.querySelectorAll(".food_trade_select").forEach((select) => {
     $(select).select2()
   })
 }
-
-// window.initFoodTradeSelect2 = function(ingredientName) {
-//   if (ingredientName) {
-    // console.log(ingredientName);
-    // $(`.food_trade_${ingredientName}`).select2({
-    //   default: ingredientName,
-    // });
-//   }
-// };
 
 export { initSelect2 };
