2edc1dce9508dfba8676e24c9ef817811b496f0c
Elie Hymowitz <ehymowitz@gmail.com>
2020-06-02 17:05:45 -0400

    Adding works

--- app/controllers/pantry_items_controller.rb
+++ app/controllers/pantry_items_controller.rb
@@ -2,19 +2,22 @@ class PantryItemsController < ApplicationController
   def index
     @pantry_items = PantryItem.includes([:ingredient]).where(user: current_user)
     @pantry_item = PantryItem.new
+
+    @addable_items = Ingredient.all.map { |ing| [ing.name, ing.id] }.sort!
   end
 
   def create
-    @pantry_item = PantryItem.new(pantry_item_params)
+    @pantry_item = PantryItem.new
     @pantry_item.user = current_user
+    @pantry_item.ingredient_id = pantry_item_params[:ingredient_id]
 
     if @pantry_item.save
-      UserOwnedIngredient.find_or_create_by(user: current_user, ingredient_id: params[:pantry_item][:ingredient_id])
-      
-      redirect_to pantry "/users/:user_id/pantry_items"
+      UserOwnedIngredient.find_or_create_by!(user: current_user, ingredient_id: params[:pantry_item][:ingredient_id])
+
+      redirect_to pantry_items_path
     else
       # Redirect to pantry_items index page for now because don't know how to render popup
-      render "/users/:user_id/pantry_items"
+      redirect_to pantry_items_path
     end
   end
 
