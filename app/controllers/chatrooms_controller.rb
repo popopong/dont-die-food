68427c7446b23b26da928ff9c2438aab210830d8
Stephanie Diep <stephaniediep@outlook.com>
2020-06-03 19:23:35 -0400

    Fixed pundit for chatroom index

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -5,6 +5,7 @@ class ChatroomsController < ApplicationController
                        .order(created_at: :desc)
                        .where("sender_id = ? OR receiver_id = ?", current_user.id, current_user.id)
                        .uniq{ |message| message.sender_id && message.chatroom_id }
+    authorize @chatroom = policy_scope(Chatroom)
   end
 
   def show
