0dfefb4b44286f1b366e858f5c9cc114522d73cd
Poyan Ng <poyanng.npy@gmail.com>
2020-06-03 17:54:44 -0400

    changing branch

--- config/routes.rb
+++ config/routes.rb
@@ -72,4 +72,8 @@ Rails.application.routes.draw do
   resources :messages, only: :create
 
   resources :users, only: [:show, :edit, :update]
+
+  MyApp::Application.routes.draw do
+    get "*any", via: :all, to: "errors#not_found"
+  end
 end
