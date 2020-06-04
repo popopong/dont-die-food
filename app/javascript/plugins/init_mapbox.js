2bebad2af5ed482609c41a1fb86cb1f62cf3c995
Stephanie Diep <stephaniediep@outlook.com>
2020-06-04 14:17:19 -0400

    Added default pictures

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -32,7 +32,6 @@ const initMapbox = () => {
     };
     
     const fitMapToMarkers = (map, markers) => {
-      console.log(map);
       const bounds = new mapboxgl.LngLatBounds();
       markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
       map.fitBounds(bounds, { padding: 70, maxZoom: 15, minZoom: 15 });
